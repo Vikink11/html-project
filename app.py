238aee33effb9d8687156db23f28e3797a526a76
vighneshwari n <vighneshwari.nk.222@gmail.com>
2026-03-09 20:31:35 +0530

    Added HTML form for add program

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Python Add Two Numbers API"
+    return render_template("program.html")
 
-@app.route('/add', methods=['GET'])
+@app.route('/add', methods=['POST'])
 def add():
-    num1 = int(request.args.get('num1'))
-    num2 = int(request.args.get('num2'))
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
     result = num1 + num2
-    return jsonify({"result": result})
+    return "Result: " + str(result)
 
 if __name__ == '__main__':
     app.run()
